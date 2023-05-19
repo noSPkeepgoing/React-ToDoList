93129895fc4ef7bc09cfd27d17fa5b644dd48824
sunpaaaa <sunpaaaa@gmail.com>
2023-05-20 00:12:22 +0900

    Add header for filtering todos item

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: '123', text: 'hi there', status: 'active' },
     { id: '456', text: 'how r u', status: 'active' },
@@ -12,10 +12,12 @@ export default function TodoList() {
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  const filtered = getFilteredItems(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -27,4 +29,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter == 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
